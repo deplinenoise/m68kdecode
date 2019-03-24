5623a7d1c36cb557d437a9e4e1244b38c1d6d6d3
Andreas Fredriksson <deplinenoise@gmail.com>
2019-03-23 23:55:09 -0700

    ftan

--- tests/decode_tests.rs
+++ tests/decode_tests.rs
@@ -6338,4 +6338,46 @@ mod tests {
             &[" fdsub.l (a0),fp1"],
         );
     }
+    //  ftan fp3
+    #[test]
+    fn test_decode_0449_ftan_fp3() {
+        test_decoding_result_ok(
+            &[0xf2, 0x00, 0x0d, 0x8f],
+            Instruction {
+                size: 10,
+                operation: FTAN,
+                operands: [FR(FP3), FR(FP3)],
+                extra: FloatFormat(FPF_EXTENDED_REAL),
+            },
+            &[" ftan fp3"],
+        );
+    }
+    //  ftan fp0,fp1
+    #[test]
+    fn test_decode_0450_ftan_fp0_fp1() {
+        test_decoding_result_ok(
+            &[0xf2, 0x00, 0x00, 0x8f],
+            Instruction {
+                size: 10,
+                operation: FTAN,
+                operands: [FR(FP0), FR(FP1)],
+                extra: FloatFormat(FPF_EXTENDED_REAL),
+            },
+            &[" ftan fp0,fp1"],
+        );
+    }
+    //  ftan.s (a0),fp1
+    #[test]
+    fn test_decode_0451_ftan_s_a0_fp1() {
+        test_decoding_result_ok(
+            &[0xf2, 0x10, 0x44, 0x8f],
+            Instruction {
+                size: 4,
+                operation: FTAN,
+                operands: [ARIND(A0), FR(FP1)],
+                extra: FloatFormat(FPF_SINGLE),
+            },
+            &[" ftan.s (a0),fp1"],
+        );
+    }
 }

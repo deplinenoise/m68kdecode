6304e652e0ee2c30241d570dd13f90e49e174687
Andreas Fredriksson <deplinenoise@gmail.com>
2019-03-23 23:35:20 -0700

    frem

--- tests/decode_tests.rs
+++ tests/decode_tests.rs
@@ -5974,9 +5974,37 @@ mod tests {
             &[" fnop"],
         );
     }
+    //  frem fp0,fp1
+    #[test]
+    fn test_decode_0423_frem_fp0_fp1() {
+        test_decoding_result_ok(
+            &[0xf2, 0x00, 0x00, 0xa5],
+            Instruction {
+                size: 10,
+                operation: FREM,
+                operands: [FR(FP0), FR(FP1)],
+                extra: FloatFormat(FPF_EXTENDED_REAL),
+            },
+            &[" frem fp0,fp1"],
+        );
+    }
+    //  frem.s (a0),fp1
+    #[test]
+    fn test_decode_0424_frem_s_a0_fp1() {
+        test_decoding_result_ok(
+            &[0xf2, 0x10, 0x44, 0xa5],
+            Instruction {
+                size: 4,
+                operation: FREM,
+                operands: [ARIND(A0), FR(FP1)],
+                extra: FloatFormat(FPF_SINGLE),
+            },
+            &[" frem.s (a0),fp1"],
+        );
+    }
     //  fsin fp3
     #[test]
-    fn test_decode_0423_fsin_fp3() {
+    fn test_decode_0425_fsin_fp3() {
         test_decoding_result_ok(
             &[0xf2, 0x00, 0x0d, 0x8e],
             Instruction {
@@ -5990,7 +6018,7 @@ mod tests {
     }
     //  fsin fp0,fp1
     #[test]
-    fn test_decode_0424_fsin_fp0_fp1() {
+    fn test_decode_0426_fsin_fp0_fp1() {
         test_decoding_result_ok(
             &[0xf2, 0x00, 0x00, 0x8e],
             Instruction {
@@ -6004,7 +6032,7 @@ mod tests {
     }
     //  fsin.s (a0),fp1
     #[test]
-    fn test_decode_0425_fsin_s_a0_fp1() {
+    fn test_decode_0427_fsin_s_a0_fp1() {
         test_decoding_result_ok(
             &[0xf2, 0x10, 0x44, 0x8e],
             Instruction {
